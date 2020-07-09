0c16541eb1a199ced74ce846febd28a234b97c84
zz987555 <hbzhuzhu2008@163.com>
2020-07-09 17:24:29 +0800

    day3起飞pojo注释少量字段

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @ResponseBody
+    @RequestMapping("/index")
+    public String index(){
+
+        return "hello user";
+    }
+
+    @ResponseBody
+    @RequestMapping("getAllUser")
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers = userService.getAllUser();
+
+        return umsMembers;
+
+    }
+
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        List<UmsMemberReceiveAddress> umsMembersReceiveAddress = userService.getReveiveAddressByMemberId(memberId);
+
+        return umsMembersReceiveAddress;
+    }
+}
